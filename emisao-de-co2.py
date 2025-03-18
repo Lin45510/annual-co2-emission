c9b8fe812680dd9bcaf18b3d52a0c311f3902a85
Y Lin N <152435819+Lin45510@users.noreply.github.com>
2025-03-18 14:29:16 -0300

    Final code

--- emisao-de-co2.py
+++ emisao-de-co2.py
@@ -1,4 +1,17 @@
 import numpy as np
 
 data = np.loadtxt('annual-co2-emissions.csv', delimiter=',', usecols=(3), skiprows=1)
-print(data)
+
+print('Dados sobre a Emissão de CO² entre 1750 e 2023')
+
+#Media
+print(f'Media: {np.mean(data):.2f}')
+
+#Mediana
+print(f'Mediana: {np.median(data):.2f}')
+
+#Desvio padrão
+print(f'Desvio Padrão: {np.std(data):.2f}')
+
+#Maxima e Minima
+print(f'Maxima: {data.max()} | Minima: {data.min()}')
